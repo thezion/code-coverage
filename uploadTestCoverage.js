457fba2f6a723e25e300248df4262a4c60c2ffd0
EC2 Default User <ec2-user@ip-172-31-36-237.ec2.internal>
2021-08-02 22:43:42 +0000

    test status

--- uploadTestCoverage.js
+++ uploadTestCoverage.js
@@ -2,6 +2,44 @@ const fs = require("fs");
 
 const report = JSON.parse(fs.readFileSync('./coverage/coverage-summary.json'));
 
-const summary = `lines:${report.total.lines.pct}% statements:${report.total.statements.pct}% functions:${report.total.functions.pct}% branches:${report.total.branches.pct}%`;
+const description = `lines:${report.total.lines.pct}% statements:${report.total.statements.pct}% functions:${report.total.functions.pct}% branches:${report.total.branches.pct}%`;
 
-console.log(summary);
+const https = require('https')
+
+const data = new TextEncoder().encode(
+  JSON.stringify({
+      "state":"success",
+      "context": "Test Coverage",
+      "description": description
+  })
+)
+
+const options = {
+  hostname: 'api.github.com',
+  port: 443,
+  path: `/repos/thezion/code-coverage/statuses${process.env.CIRCLE_SHA1}`,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+    'Authorization': 'Basic ' + new Buffer(process.env.GITHUB_USER_AND_TOKEN).toString('base64')
+  }
+}
+
+console.log(`Requesting ${options.path}`)
+console.log(`Psw = ${process.env.GITHUB_USER_AND_TOKEN.substr(0,10)}`)
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
